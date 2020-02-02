42ef95c25c78db20ac6089855bad4591b042956c
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2020-02-02 11:19:58 +0000

    Changed all references of "WindowsFormsApp9" to "SharpLocker"
    Also renamed the folder the project is from "WindowsFormsApp9" to "SharpLocker"

--- SharpLocker/Program.cs
+++ SharpLocker/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp9
+namespace SharpLocker
 {
     static class Program
     {
--- SharpLocker/Properties/Settings.Designer.cs
+++ SharpLocker/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsApp9.Properties
+namespace SharpLocker.Properties
 {
 
 
