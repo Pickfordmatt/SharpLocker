c0990c91f9632783f9fa74a93450d55efa45dec0
3top1a <57371001+3top1a@users.noreply.github.com>
2020-02-09 13:47:48 +0100

    Profile picture detection, background and username stuff

--- SharpLocker/LockScreenForm.cs
+++ SharpLocker/LockScreenForm.cs
@@ -4,44 +4,86 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing.Drawing2D;
 
 namespace SharpLocker
 {
     public partial class LockScreenForm : Form
     {
+        [DllImport("shell32.dll", EntryPoint = "#261",
+        CharSet = CharSet.Unicode, PreserveSig = false)]
+        public static extern void GetUserTilePath(
+        string username,
+        UInt32 whatever, // 0x80000000
+        StringBuilder picpath, int maxLength);
+
+        public static string GetUserTilePath(string username)
+        {   // username: use null for current user
+            var sb = new StringBuilder(1000);
+            GetUserTilePath(username, 0x80000000, sb, sb.Capacity);
+            return sb.ToString();
+        }
+
+        public static Image GetUserTile(string username)
+        {
+            return Image.FromFile(GetUserTilePath(username));
+        }
+
         public LockScreenForm()
         {
             InitializeComponent();
             Taskbar.Hide();
-            
+
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Normal;
             StartPosition = FormStartPosition.Manual;
             Location = new Point(0, 0);
             Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-            
-            Image myimage = new Bitmap(@"C:\Windows\Web\Wallpaper\Windows\img0.jpg");
+
+            //Creds to keldnorman
+            //https://github.com/Pickfordmatt/SharpLocker/issues/2
+            Image myimage = new Bitmap(@Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Microsoft\\Windows\\Themes\\TranscodedWallpaper"));
             BackgroundImage = myimage;
+
             BackgroundImageLayout = ImageLayout.Stretch;
             TopMost = true;
-            
+
             string userName = Environment.UserName;
             UserNameLabel.Text = userName;
             UserNameLabel.BackColor = Color.Transparent;
-            
+
             int usernameloch = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 64;
             int usericonh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 29;
             int buttonh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 64;
             int usernameh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 50;
             int locked = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 57;
-           
+
             PasswordTextBox.Top = usernameloch;
             PasswordTextBox.UseSystemPasswordChar = true;
             ProfileIcon.Top = usericonh;
             SubmitPasswordButton.Top = buttonh;
             UserNameLabel.Top = usernameh;
             LockedLabel.Top = locked;
-            
+            PasswordTextBox.UseSystemPasswordChar = true;
+
+            //Get the username. This returns Domain\Username
+            string userNameText = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+            //Set the text
+            UserNameLabel.Text = userNameText.Split('\\')[1];
+
+            //https://stackoverflow.com/questions/7731855/rounded-edges-in-picturebox-c-sharp
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddEllipse(0, 0, ProfileIcon.Width - 3, ProfileIcon.Height - 3);
+            Region rg = new Region(gp);
+            ProfileIcon.Region = rg;
+            ProfileIcon.Image = GetUserTile(userNameText.Split('\\')[1]);
+
+
+
             foreach (var screen in Screen.AllScreens)
             {
                 Thread thread = new Thread(() => WorkThreadFunction(screen));
@@ -86,7 +128,7 @@ namespace SharpLocker
                 return parms;
             }
         }
-        
+
         protected override void OnClosing(CancelEventArgs e)
         {
             Taskbar.Show();
